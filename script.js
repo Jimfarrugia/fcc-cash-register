53775d76e3cd3ed7f1a4ed1c268875376a46350e
Jim <the.jim.farrugia@gmail.com>
2021-08-26 09:56:22 +1000

    define variables

--- script.js
+++ script.js
@@ -17,27 +17,46 @@
 // Otherwise, return {status: "OPEN", change: [...]}, with the change due in
 // coins and bills, sorted in highest to lowest order, as the value of the change key.
 
-//* Thoughts:
-// use an object to store note/coin denominations ("PENNY": 0.01).
-// subtract price from cash to find change owed.
-// loop through cid from highest to lowest denominations.
-//   if a denomination is <= change owed and is > 0 within cid,
-//   remove one of that denomination from cid and add it to result.change.
-// return the appropriate status and change in the result object.
-
 function checkCashRegister(price, cash, cid) {
-  var change;
-  return change;
+  // use an object to store note/coin denominations ("PENNY": 0.01).
+  const denominations = {
+    PENNY: 0.01,
+    NICKEL: 0.05,
+    DIME: 0.1,
+    QUARTER: 0.25,
+    DOLLAR: 1,
+    FIVE: 5,
+    TEN: 10,
+    TWENTY: 20,
+    "ONE HUNDRED": 100,
+  };
+
+  // subtract price from cash to find change owed.
+  const changeOwed = cash - price;
+
+  let changeInHand = [];
+
+  // loop through cid from highest to lowest denominations.
+  for (let i = cid.length - 1; i >= 0; i--) {
+    console.log(i);
+    // TODO - if a denomination is <= change owed and is available (> 0) within cid,
+    // TODO   then remove one of that denomination from cid and add it to changeInHand.
+  }
+
+  // TODO - return an object with the appropriate status and change
+  return changeOwed;
 }
 
-checkCashRegister(19.5, 20, [
-  ["PENNY", 1.01],
-  ["NICKEL", 2.05],
-  ["DIME", 3.1],
-  ["QUARTER", 4.25],
-  ["ONE", 90],
-  ["FIVE", 55],
-  ["TEN", 20],
-  ["TWENTY", 60],
-  ["ONE HUNDRED", 100],
-]);
+console.log(
+  checkCashRegister(19.5, 20, [
+    ["PENNY", 1.01],
+    ["NICKEL", 2.05],
+    ["DIME", 3.1],
+    ["QUARTER", 4.25],
+    ["ONE", 90],
+    ["FIVE", 55],
+    ["TEN", 20],
+    ["TWENTY", 60],
+    ["ONE HUNDRED", 100],
+  ])
+);
